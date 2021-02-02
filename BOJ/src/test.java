7e4a3c7e46bff1e3fab0f81208ef7764a015e1bc
kwanheumcho <rhksgma258@gmail.com>
2021-02-02 23:29:24 +0900

    폴더생성

--- BOJ/src/test.java
+++ BOJ/src/test.java
@@ -3,12 +3,6 @@ import java.util.ArrayList;
 
 public class test {
 	public static void main(String[] args) throws IOException {
-		ArrayList al = new ArrayList();
-		al.add("È£¶ûÀÌ");
-		al.add(3);
-		System.out.println(al.size());
-		System.out.println(al.get(1));
-		System.out.println(al.indexOf("È£¶ûÀÌ"));
-		System.out.println(al.contains("È£¶ûÀÌ"));
+		
 	}
 }
