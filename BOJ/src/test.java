113fa4e5f313dd5e4455d8f36aba490072a9298d
kwanheumcho <rhksgma258@gmail.com>
2021-02-07 16:18:22 +0900

    2021.02.07

--- BOJ/src/test.java
+++ BOJ/src/test.java
@@ -1,10 +1,18 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class test {
 	public static void main(String[] args) throws IOException {
-		double x = Double.MAX_VALUE;
-		double y = Double.MAX_VALUE;
-		System.out.println( (x + y/2) /2);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String x = br.readLine();
+		char[] ch = x.toCharArray();
+		for(char c : ch) {
+			System.out.println(c);
+		}
+		StringTokenizer st = new StringTokenizer(x, "_");
+		//StringTokenizer stk = new StringTokenizer()
+		System.out.println(st.countTokens());
 	}
 }
